8e8e89c40fdb0904bebaccde54e9879d804b6bad
zhanba <c5e1856@gmail.com>
2018-05-31 00:51:29 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import { Observable, of, interval } from 'rxjs'
-import { map, repeatWhen } from 'rxjs/operators'
+import { Observable, of, interval, timer, merge } from 'rxjs'
+import { repeatWhen, map, zip } from 'rxjs/operators'
 
-const notifier = () => {
-    return interval(1000)
-}
+// const notifier = () => {
+//     return interval(1000)
+// }
 
-const source$ = of(1, 2, 3).repeatWhen(notifier)
+// const source$ = of(1, 2, 3).pipe(repeatWhen(notifier))
+const source1$ = timer(0, 1000).pipe(map(x => x + 'A'))
+const source2$ = timer(500, 1000).pipe(map(x => x + 'B'))
 
+// const source$ = merge(source1$, source2$)
+const source$ = source1$.pipe(zip(source2$))
 
 source$.subscribe(x => console.log(x))
